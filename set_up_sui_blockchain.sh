515b52285aea6db5518d56015539ae7e18b4eac3
root <root@DESKTOP-AEUJT5S>
2024-03-20 00:50:31 +0530

    sui set up script added

---
+++ set_up_sui_blockchain.sh
@@ -0,0 +1,14 @@
+sudo apt-get update -y
+sudo apt install curl -y
+sudo apt-get install git-all -y
+sudo apt-get install cmake -y
+sudo apt-get install gcc -y
+sudo apt-get install libssl-dev -y
+# sudo apt-get install pkg-config -y uncomment it if you dont have openssl in your linux distro
+sudo apt-get install libclang-dev -y
+sudo apt-get install libpq-dev -y
+sudo apt-get install build-essential -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -y  
+cargo install --locked --git https://github.com/MystenLabs/sui.git --branch devnet sui -y
+
+
